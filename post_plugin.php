3ec9d39203a3efefc299726e6302414bb0a3c45c
Cleiton Pereira <cleiton.workspace@gmail.com>
2017-11-17 17:31:49 -0200

    Alteracao no nome do plugin

--- post_plugin.php
+++ post_plugin.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Custom Post Widget
+Plugin Name: FDO Single Post Widget
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A widget to show a single post.
+Description: A widget a single post content.
 Version: 0.1
 Author: Cleiton Pereira
 Author URI: http://URI_Of_The_Plugin_Author
